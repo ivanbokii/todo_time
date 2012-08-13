bbdf3329f82ad3f4f9a62fbbf48979665759113e
ivanbokii <bokiyis@gmail.com>
2012-08-14 00:25:55 +0300

    modified rakefile to compile minified and combined versions to min folder

--- lib/rakefile.rb
+++ lib/rakefile.rb
@@ -1,3 +1,4 @@
 task :default do
-  exec 'node r.js -o baseUrl=. name=almond.js include=app.js out=../min/todo_time.js wrap=true optimize=none'
+  %x[node r.js -o baseUrl=. name=almond.js include=app.js out=../min/todo_time.js wrap=true optimize=none]
+  %x[node r.js -o baseUrl=. name=almond.js include=app.js out=../min/todo_time.min.js wrap=true]
 end

4afb805c6ef467dfbc68d8858c5b6ca7b0035dac
ivanbokii <bokiyis@gmail.com>
2012-08-14 12:19:05 +0300

    removed all derivative *.js files from project

--- lib/rakefile.rb
+++ lib/rakefile.rb
@@ -1,4 +1,6 @@
 task :default do
-  %x[node r.js -o baseUrl=. name=almond.js include=app.js out=../min/todo_time.js wrap=true optimize=none]
-  %x[node r.js -o baseUrl=. name=almond.js include=app.js out=../min/todo_time.min.js wrap=true]
+  %x[coffee -c -o ./ ../src/]
+
+  %x[node vendor/r.js -o baseUrl=. name=vendor/almond.js include=app.js out=../min/todo_time.js wrap=true optimize=none]
+  %x[node vendor/r.js -o baseUrl=. name=vendor/almond.js include=app.js out=../min/todo_time.min.js wrap=true]
 end
